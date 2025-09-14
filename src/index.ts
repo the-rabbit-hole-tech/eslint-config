cde459dc050c04d773f62fc2e9da1e64c8ee32f3
Bugs5382 <bugs5382@users.noreply.github.com>
2025-09-14 11:02:44 -0400

    feat: ability to turn off eslint plugins

--- src/index.ts
+++ src/index.ts
@@ -24,26 +24,58 @@ export const globalIgnoresArray = [
 ];
 
 /**
+ * Base extends with string keys
+ * @description These are items that can be disabled.
+ * @example
+ * - eslintPerfectionist
+ * - eslintTypescript
+ * - eslintReact
+ * - eslintA11y
+ * - eslintTesting
+ * - eslintPrettier
+ * - eslintStorybook
+ * @since 1.0.0
+ */
+const baseExtendsMap = {
+  eslintA11y: eslintA11y.recommended,
+  eslintPerfectionist,
+  eslintPrettier,
+  eslintReact,
+  eslintStorybook: eslintStorybook["flat/recommended"],
+  eslintTesting,
+  eslintTypescript: eslintTypescript.recommended,
+} as const;
+
+/**
+ * Factory to create ESLint config
+ * @description Review for more info.
+ * @since 1.0.0
+ * @param options List of extend names (keys) to remove from base config
+ */
+export function createESLintConfig(options?: {
+  disableExtends?: (keyof typeof baseExtendsMap)[];
+}) {
+  const disabled = options?.disableExtends ?? [];
+  return defineConfig([
+    {
+      ignores: globalIgnoresArray,
+    },
+    {
+      extends: Object.entries(baseExtendsMap)
+        .filter(
+          ([key]) => !disabled.includes(key as keyof typeof baseExtendsMap),
+        )
+        .map(([, value]) => value),
+      rules: {
+        "react/react-in-jsx-scope": "off",
+      },
+    },
+  ]);
+}
+
+/**
  * Default Config
  * @description Used for all 'the rabbit hole' projects.
  * @since 1.0.0
  */
-export default defineConfig([
-  {
-    ignores: globalIgnoresArray,
-  },
-  {
-    extends: [
-      eslintPerfectionist,
-      eslintTypescript.recommended,
-      eslintReact,
-      eslintA11y.recommended,
-      eslintTesting,
-      eslintPrettier,
-      eslintStorybook["flat/recommended"],
-    ],
-    rules: {
-      "react/react-in-jsx-scope": "off",
-    },
-  },
-]);
+export default createESLintConfig();
